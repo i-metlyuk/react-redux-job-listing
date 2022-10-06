aa369d1051215e83d73d3a9664909c52500fe387
i-metlyuk <igor.m@core-tech.ru>
2022-10-06 13:04:31 +0300

    add redux persist feature

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react'
+
 import './index.css';
 import App from './App';
-import {Provider} from "react-redux";
-import {store} from 'store/index';
+
+import {persistor, store} from 'store/index';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
+        <Provider store={store}>,
+            <PersistGate loading={null} persistor={persistor}>
+                <App/>
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
 import {createStore} from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
 import {rootReducer} from "./rootReducer";
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  // whitelist: ["filters", "positions"],
+  // blacklist: []
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export const persistor = persistStore(store);
